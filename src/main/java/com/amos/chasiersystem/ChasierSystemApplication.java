6ec347fd0a5064b529b959f99d32a9b57bbd35a6
Amos Sitorus <amos.sitorus@advotics.com>
2021-03-05 20:24:18 +0700

    init project

---
+++ src/main/java/com/amos/chasiersystem/ChasierSystemApplication.java
@@ -0,0 +1,13 @@
+package com.amos.chasiersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChasierSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChasierSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amos/chasiersystem/ChasierSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amos.chasiersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChasierSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
